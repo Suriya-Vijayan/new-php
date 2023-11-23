bee8010b4e4636ab43dd5b43ff55e310afedf4c2
Suriya-Vijayan <suriyavijayank@gmail.com>
2023-11-23 11:47:39 +0000

    initial commit

---
+++ db.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_SERVER', 'database-1.ct5mcpxk6p0v.eu-west-2.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin321');
+define('DB_DATABASE', 'php');
+?>
